769aa671d24f3509e7a9a0ea6f46584f0418f1f2
Moises Hernandez Sanchez <telekineticjake@gmail.com>
2022-09-14 11:50:19 -0400

    Completed piano w/ key remaps

---
+++ app.js
@@ -0,0 +1,39 @@
+const pianoKeys = document.querySelectorAll('.key')
+
+pianoKeys.forEach((pianoKey, i) => {
+  const reMap = [
+    '02',
+    '04',
+    '06',
+    '09',
+    '11',
+    '14',
+    '16',
+    '18',
+    '21',
+    '23',
+    '01',
+    '03',
+    '05',
+    '07',
+    '08',
+    '10',
+    '12',
+    '13',
+    '15',
+    '17',
+    '19',
+    '20',
+    '22',
+    '24',
+  ]
+  // const number = i < 9 ? `0${i + 1}` : i + 1
+  // const newUrl = `keys-audio/key${number}.mp3`
+  const newUrl = `keys-audio/key${reMap[i]}.mp3`
+  pianoKey.addEventListener('click', () => playSound(newUrl))
+})
+
+function playSound(newUrl) {
+  new Audio(newUrl).play()
+  console.log(newUrl)
+}
